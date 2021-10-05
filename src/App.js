57c0a0a4e40cffe565a78a62db8dd6df0940c5f1
OlaShabalina <o.shabalina@outlook.com>
2021-10-05 21:15:19 +1100

    created a pokecard component to display an indivisual Pokemon

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import Pokecard from './Pokecard';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-        <Pokegame />
+        <Pokecard 
+        id={4}
+        name='Charmander'
+        type='fire'
+        exp={62}
+        />
     </div>
   );
 }
